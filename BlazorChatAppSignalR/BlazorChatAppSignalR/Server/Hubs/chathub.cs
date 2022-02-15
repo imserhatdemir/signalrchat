3fbe1397b82151ab3c6d5559a11aa794c0169684
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2022-02-15 23:01:13 +0300

    signalr

---
+++ BlazorChatAppSignalR/BlazorChatAppSignalR/Server/Hubs/chathub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatAppSignalR.Server.Hubs
+{
+    public class chathub : Hub
+
+    {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+
+        public override async Task OnConnectedAsync()
+        {
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            await AddMessageToChat(string.Empty, $"{username} kullanıcısı iniş yaptı");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username=Users.FirstOrDefault(u=>u.Key==Context.ConnectionId).Value;
+            await AddMessageToChat(string.Empty, $"{username} çıkış yaptı");
+        }
+
+        public async Task AddMessageToChat(string user, string message)
+        {
+            await Clients.All.SendAsync("GetThatMessageDude",user, message);
+        }
+    }
+}
